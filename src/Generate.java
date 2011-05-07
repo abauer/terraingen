1f27f6e1fa37f9f2df02b73d4366229067c3827d
TexasGamer <tpgaubert@gmail.com>
2011-05-07 11:03:02 -0500

    clean up code

--- src/Generate.java
+++ src/Generate.java
@@ -50,48 +50,100 @@ public class Generate
 	public static void getSeed()
 	{
 		seed = Expo.enterIntGUI("Enter Seed (Leave blank for random seed)");
+		Generate.seeds();
+	}
+
+	public static void seeds();
+	{
+		// Makes seeds from seeds
+		// See documentation for an explanation
+
+		/*
+		 * Make random # from entered seed
+		 *
+		 * Use that # as seed for new random #
+		 *
+		 * If first rand. # > second rand. # --> subtract 1st # from 2nd # to get a third # (else do vice versa)
+		 *
+		 * Take 3rd # and make another rand # with it
+		 *
+		 * Compare the 3rd number to 1st and 2nd #'s (separately) and do same subtration as above to make 4th and 5th random #
+		 *
+		 * That should be it??
+		 *
+		 */
 	}
 
 	public static void draw()
 	{
 		Generate.setConstants();
+		Generate.assignRandom();
 	}
 
 	private static void setConstants()
 	{
-		// First four rows with "air"
+		// First five rows with "air"
 		Arrays.fill(row0,0);
 		Arrays.fill(row1,0);
 		Arrays.fill(row2,0);
 		Arrays.fill(row3,0);
+		Arrays.fill(row4,0);
 
 		// First two land rows with "dirt"
-		Arrays.fill(row4,1);
 		Arrays.fill(row5,1);
+		Arrays.fill(row6,1);
+		Arrays.fill(row7,1);
+		Arrays.fill(row8,1);
 
 		// Final 2 rows with "bedrock"
 		Arrays.fill(row24,7);
 		Arrays.fill(row25,7);
 
 		// All others with "rock" (for now)
-		Arrays.fill(row6,2);
-		Arrays.fill(row7,2);
-		Arrays.fill(row8,2);
+
+		// Intermediate Zone
 		Arrays.fill(row9,2);
 		Arrays.fill(row10,2);
+		// Rock: 75%, Diamond: 0%, Iron: 25%, Redstone: 0%
 		Arrays.fill(row11,2);
 		Arrays.fill(row12,2);
 		Arrays.fill(row13,2);
 		Arrays.fill(row14,2);
+		// Rock: 50%, Diamond: 25%, Iron: 0%, Redstone: 25%
 		Arrays.fill(row15,2);
 		Arrays.fill(row16,2);
 		Arrays.fill(row17,2);
 		Arrays.fill(row18,2);
+		// Rock: 50%, Diamond: 25%, Iron: 0%, Redstone: 0%
 		Arrays.fill(row19,2);
 		Arrays.fill(row20,2);
 		Arrays.fill(row21,2);
 		Arrays.fill(row22,2);
 		Arrays.fill(row23,2);
 	}
+
+	private static void assignRandom()
+	{
+		/////////////////////
+		//Intermediate Zone//
+		/////////////////////
+
+		//row9
+
+		//row10
+
+		/////////////////////
+		//Top Rock Zone    //
+		/////////////////////
+
+		/////////////////////
+		//Mid Rock Zone    //
+		/////////////////////
+
+		/////////////////////
+		//Lower Rock Zone  //
+		/////////////////////
+	}
+
 
 }
