4f9a3787e7d1e353bed4d86ed46d59e72a3b2823
TexasGamer <tpgaubert@gmail.com>
2011-05-06 21:17:55 -0500

    initial commit of Menu.java

---
+++ src/Menu.java
@@ -0,0 +1,19 @@
+public class Menu
+{
+
+	public static void drawMain()
+	{
+		// Title and the such will be drawn using this method
+	}
+
+	public static void drawDirections()
+	{
+		// Directions will be drawn using this method
+	}
+
+	public static void drawDone()
+	{
+		Expo.displayGUI("Generation complete. You may dismiss this popup and enjoy the landscape. To make another, reload program.");
+	}
+
+}
