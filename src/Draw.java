ce4a14be33a97f5e92af0bc789047c0588bddc16
TexasGamer <tpgaubert@gmail.com>
2011-05-07 15:16:46 -0500

    initial commit of Draw.java

---
+++ src/Draw.java
@@ -0,0 +1,22 @@
+// Draw.java
+//
+// Reads the arrays' indexes
+// Draws the blocks on the screen
+
+import Java.util.Arrays;
+
+public class Draw
+{
+	public static void draw()
+	{
+		Generate.setConstants();
+		Generate.assignRandom();
+		Draw.go();
+	}
+
+	public static void go()
+	{
+
+	}
+
+}
